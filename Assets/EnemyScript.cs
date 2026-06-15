08d194aa71d328a4f8143a502dffcfa30600b76a
karlaps45 <140808213+karlaps45@users.noreply.github.com>
2026-06-16 01:50:54 +0200

    AMYOUR

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -2,29 +2,15 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    public Animator animator;
     public AnimationClip death;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.tag = "bullet")
+        if (collision.gameObject.tag == "bullet")
         {
-
+            animator.Play("DIE");
+            Destroy(gameObject, death.length);
         }
     }
 }
-
-
-
--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -16,4 +16,13 @@ public class bullet : MonoBehaviour
     {
         transform.position += direction * speed * Time.deltaTime;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
